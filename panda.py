6092aaa876f028307187498c8b6ad960c4e1e396
qeishfai <4131240416_qeish@pknstan.ac.id>
2025-02-12 12:31:46 +0700

    Update panda.py

--- panda.py
+++ panda.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import sqlite3
 import pandas as pd
-st.title("hello panda")
-st.write("panda pindi")
-
-st.title("BUNGA")
-st.image("https://upload.wikimedia.org/wikipedia/commons/b/bd/Helianthus_annuus_exposed_2004-05-22.jpg")
 
 def main():
     st.set_page_config(page_title="Belajar SQL", layout="wide")
