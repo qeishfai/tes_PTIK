94cf7738254d72ab55111891519ea36f50244848
qeishfai <4131240416_qeish@pknstan.ac.id>
2025-02-12 12:23:29 +0700

    Update panda.py

--- panda.py
+++ panda.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.title("hello panda")
 st.write("panda pindi")
+
+st.title("BUNGA")
+st.image("https://upload.wikimedia.org/wikipedia/commons/b/bd/Helianthus_annuus_exposed_2004-05-22.jpg")
